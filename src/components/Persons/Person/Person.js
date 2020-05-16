dd3fde5b0f2d459a9f1f542a9f9115a23b5def7b
Anton Smirnov <antosha-85@yandex.ru>
2020-05-15 23:07:47 -0600

    Coverted person and persons from function to a class based components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,43 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Person.module.css";
 // import styled from 'styled-components';
 // import Radium from 'radium';
+class Person extends Component {
+  render() {
 
-const person = (props) => {
-  console.log('[Person.js] rendering ... ')
-  // const StyledDiv = styled.div`
-  // width: 60%;
-  // margin: 16px auto;
-  // border: 1px solid #eee;
-  // box-shadow: 0 2px 3px #ccc;
-  // padding: 16px;
-  // text-align: center ;
-
-  // @media (min-width: 500px) {
-  //     width: 450px;
-  // }
-  // `;
-
-  // const style = {
-  //     '@media (min-width: 500px)': {
-  //         width: '450px'
-  //     }
-  // }
-  // const rnd = Math.random();
-
-  // if (rnd > 0.7) {
-  //   throw new Error('Something went wrong!')
-  // }
-  return (
-    <div className={classes.Person}>
-      <h2 onClick={props.click}>
-        I'm a {props.name} and I am {props.age} years old!
-      </h2>
-      <div>{props.children}</div>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
+    console.log('[Person.js] rendering ... ')
+    // const StyledDiv = styled.div`
+    // width: 60%;
+    // margin: 16px auto;
+    // border: 1px solid #eee;
+    // box-shadow: 0 2px 3px #ccc;
+    // padding: 16px;
+    // text-align: center ;
+  
+    // @media (min-width: 500px) {
+    //     width: 450px;
+    // }
+    // `;
+  
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //         width: '450px'
+    //     }
+    // }
+    // const rnd = Math.random();
+  
+    // if (rnd > 0.7) {
+    //   throw new Error('Something went wrong!')
+    // }
+    return (
+      <div className={classes.Person}>
+        <h2 onClick={this.props.click}>
+          I'm a {this.props.name} and I am {this.props.age} years old!
+        </h2>
+        <div>{this.props.children}</div>
+        <input type="text" onChange={this.props.changed} value={this.props.name} />
+      </div>
+    );
+  }
 };
 
 // export default Radium(person)
-export default person;
+export default Person;
